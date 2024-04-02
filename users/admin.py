c6305c0d231fc9b64af99e4770dda5fa73bbf56b
Pranitha <201231.pranitha@gmail.com>
2024-04-03 00:10:54 +0530

    initial 2

--- users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import FattigueInfoModel,DriverRegistrationModel
-
-# Register your models here.
-admin.site.register(FattigueInfoModel)
-admin.site.register(DriverRegistrationModel)
-
